0e3eb37bd7e7e6c6c2d3f56c7ab1a2f7d5a0e3fc
MateusSolrac <mateussolrac08@gmail.com>
2025-09-16 15:45:52 -0300

    Update cardapio.js

--- cardapio.js
+++ cardapio.js
@@ -1,16 +1,20 @@
 
 //armazenamento dos dados
 function soma_pedido(num){
-            
-
     let valor = document.getElementById("resultado").value + num 
-    
-    
     document.getElementById("resultado").value = valor + "+"
-    }
+}
+
+    
+// finalizar compra
+function comprar(){
+    document.getElementById("resultado").value = ""
+    alert("Compra em preparação !!")
+}
 
 
-    function resultado(){
+// soma dos itens
+function resultado(){
         let resultado = eval(document.getElementById("resultado").value.slice(0, -1))
         document.getElementById("resultado").value = "R$ " + resultado
-    }
+}
